9008b94d7262a82ac97db7eecf07f0adaf9c0332
Hadik <hadikcze@gmail.com>
2020-10-21 17:53:02 +0200

    Remove everythink expect build

--- src/helpers/JsonDiffHolder.js
+++
@@ -1,7 +0,0 @@
-export default class JsonDiffHolder {
-    constructor () {
-        this._lastJson = {};
-    }
-
-    getJson
-}
--- src/helpers/UrlHelpers.ts
+++
@@ -1,6 +0,0 @@
-export default class UrlHelpers {
-    static getQueryParam (paramName: string): string | null {
-        const urlParams = new URLSearchParams(window.location.search);
-        return urlParams.get(paramName);
-    }
-}
